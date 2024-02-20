f7d842900caf0b9617810d2f5ac26b08fa4ee134
khafiz12 <karimhafiz7@gmail.com>
2024-02-20 13:39:26 -0500

    Express and Morgan library
    I added express to simplify coding for request and responds to the server. Added morgan to log information to the server.

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 let express = require('express');
+let morgan = require('morgan');
 let app = express();
 
 
+let topMovies = [
+ { 
+  title: '',
+  author: '',
+  date: '',   
+ }     
+];
+
+app.use(morgan('common'));
+
+app.get('/movies', (req, res) => { 
+  res.send('Welcome to my Top 10 Movies');
+});
+
+app.get ('/', (req, res) => {
+  res.send('My favorite movie selection')  
+});
+
+app.get ('/doc.html', (req,res) => {
+  res.sendFile('Public/doc.html', {root:_dirname});
+});
+
+app.arguments(express.static('Public'));
+
+app.use((err,req,res,next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke');
+});
+
+app.listen(8080,() => {
+ console.log('Your app is listening on port 8080');   
+});
