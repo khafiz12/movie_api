cd0258b614cfb86b2735e4036e4b9f2a348703b2
khafiz12 <karimhafiz7@gmail.com>
2024-02-21 11:33:58 -0500

    fixed errors on ex 2.4.
    I was able to link the top ten movies list to local host site.

--- index.js
+++ index.js
@@ -1,35 +1,69 @@
 let express = require('express');
-let morgan = require('morgan');
+let morganRequire = require('morgan');
 let app = express();
 
 
-let topMovies = [
+let topTenMovies = [
  { 
-  title: '',
-  author: '',
-  date: '',   
- }     
+  title: 'Interstellar',
+  director: 'Christopher Nolan'   
+ },     
+ {
+  title:'John Wick',
+  director: 'Chad Stahelski'
+ },
+ {
+  title: 'John Wick:Chapter 2',
+  director: 'Chad Stehelski'
+ },
+ {
+  title:'Rambo 3',
+  director:'Peter Macdonald'
+ },
+ {
+  title:'The Dark Knight',
+  director:'Christopher Nolan'
+ },
+ {
+  title:'Scarface',
+  director:'Brian De Palma'
+ },
+ {
+  title: 'Top Gun',
+  director:'Tony Scott'
+ },
+ {
+  title:"White Men Can't Jump",
+  director:'Ron Shelton'
+ },
+ {
+  title:'Men in Black',
+  director:'Barry Sonnenfeld'
+ },
+ {title:'Saving Private Ryan',
+  director:'Steven Spielberg'
+ },
 ];
 
-app.use(morgan('common'));
+app.use(morganRequire('common'));
 
-app.get('/movies', (req, res) => { 
+app.get('/', (req, res) => { 
   res.send('Welcome to my Top 10 Movies');
 });
 
-app.get ('/', (req, res) => {
-  res.send('My favorite movie selection')  
+app.get ('/movies', (req, res) => {
+  res.json(topTenMovies)  
 });
 
 app.get ('/doc.html', (req,res) => {
   res.sendFile('Public/doc.html', {root:_dirname});
 });
 
-app.arguments(express.static('Public'));
+app.use(express.static('Public'));
 
 app.use((err,req,res,next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke');
+  res.status(500).send('Something broke')
 });
 
 app.listen(8080,() => {
